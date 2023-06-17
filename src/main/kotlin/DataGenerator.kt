a977614180747fd02085c7e88c800045a6927af8
WildPiggie <wildpiggie@gmail.com>
2023-06-17 18:58:27 +0100

    Data generator now acts as a threaded server

--- src/main/kotlin/DataGenerator.kt
+++ src/main/kotlin/DataGenerator.kt
@@ -1,39 +1,66 @@
 import java.io.IOException
-import java.io.ObjectInputStream
-import java.io.ObjectOutputStream
 import java.net.ConnectException
 import java.net.InetAddress
+import java.net.ServerSocket
 import java.net.Socket
+import java.net.http.WebSocket
 
 //throws?
-class DataGenerator(val targetAddress: InetAddress, val targetPort: Int) {
-    @Throws(IOException::class)
-    fun connectAndSendData(){
-        val socket = Socket(targetAddress, targetPort)
-        val output = ObjectOutputStream(socket.getOutputStream())
-        //val input = ObjectInputStream(socket.getInputStream())
+class DataGeneratorServer(private val serverPort: Int): Thread() {
+    var currentValue = 0
 
-        while(!socket.isClosed){
-            output.writeInt((Math.random()*100).toInt())
-            Thread.sleep(5000)
+    init {
+        ValueGenerator().start()
+    }
+
+    @Throws(IOException::class)
+    override fun run() {
+        val serverSocket = ServerSocket(serverPort)
+
+        while(!serverSocket.isClosed){
+            val clientSocket = serverSocket.accept()
+            ClientHandler(clientSocket).start()
+            println("Client connected: ${clientSocket.inetAddress.toString().drop(1)}:${clientSocket.port}")
+        }
+    }
+
+    inner class ValueGenerator: Thread() {
+        override fun run() {
+            while(this.isAlive){
+                currentValue = (Math.random()*100).toInt()
+                sleep(1000)
+            }
+        }
+    }
+
+    inner class ClientHandler(private val client: Socket): Thread() {
+        override fun run() {
+            val output = client.getOutputStream()
+            while(!client.isClosed){
+                output.write(currentValue)
+                println("Sent message to client")
+                sleep(5000)
+            }
         }
     }
 }
 
 fun main(args: Array<String>) {
-    if (args.size < 2)
-        throw IllegalArgumentException("Invalid arguments!")
-    if(args[1].toInt() < 0)
+    if (args.isEmpty())
+        throw IllegalArgumentException("No arguments provided!")
+    if(args.first().toInt() < 0)
         throw IllegalArgumentException("Invalid port number!")
 
-    val ipAddress = InetAddress.getByName(args.first())
-    val port = args[1].toInt()
+    val ipAddress = InetAddress.getLocalHost()
+    val port = args.first().toInt()
 
-    val generator = DataGenerator(ipAddress, port)
+    val generator = DataGeneratorServer(port)
 
     try {
-        generator.connectAndSendData()
+        generator.start()
     } catch (e: IOException) {
         throw ConnectException("Connection failed!")
     }
+
+    println("Server running at: ws://${ipAddress.hostAddress}:$port")
 }
